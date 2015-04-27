1b3969756cc3d63e81f7dad3bb9586e4d24eaadd
SacreDeBirmanie <sebful44@hotmail.fr>
2015-04-27 17:24:10 +0200

    fffff

--- src/melange.sh
+++ src/melange.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 cp $1 tempo1
+
+> $2
+sed -n "1 p" tempo1 >> $2
+sed -e "1 d" tempo1 >> tempo3
+mv tempo3 tempo1
+
 nbligne=$(sed -n '$=' tempo1)
 nbl=nbligne
-min=2
-> $2
+min=1
+
 while ((min <= nbligne))
 do
-    rnd=$((RANDOM%$nbl+1))
+    rnd=$((RANDOM%$nbl))
     ((rnd+=1))
-    sed -n -e "$rnd p" tempo1 >> $2
+    sed -n "$rnd p" tempo1 >> $2
     sed -e "$rnd d" tempo1 >> tempo3
     mv tempo3 tempo1
     ((min+=1))
